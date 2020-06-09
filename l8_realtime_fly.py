3b4e661f885d25e8bef81e74ae530abefd7db8e0
Lancine KANTE <kantemou@gmail.com>
2020-06-09 19:58:14 +0000

    Review l8_realtime_fly to work on jupyter notebook

--- l8_realtime_fly.py
+++ l8_realtime_fly.py
@@ -3,7 +3,7 @@
 Created on Sun May 14 14:46:24 2020
 @author: Lancine KANTE
 """
-
+#%matplotlib inline
 import landsat as lsat
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,14 +12,13 @@ import pandas as pd
 
 lsa=lsat.Landsat8()
 today_path=lsa.get_nvisite()
-gn=lsa.gn
-gn=gn[gn.DayNight=='DAY']
+gn=lsa.gn[lsa.gn.DayNight=='DAY']
 
 gn=gn[gn.ADate.between('02/01/2020','02/16/2020')]
 xs = np.array(gn.loc[:,'CenterLongdec'])
 ys = np.array(gn.loc[:,'CenterLatdec'])
 
-def update_line(num, xs, ys, line,anime=False):
+def update_sat(num, xs, ys, line,anime=False):
     if True==anime:
         line.set_xdata(xs[..., :num])
         line.set_ydata(ys[..., :num])
@@ -30,13 +29,16 @@ def update_line(num, xs, ys, line,anime=False):
 
 nframe=len(gn.LPID.unique())
 
-fig = plt.figure()
-fig.add_subplot(111)
+fig, ax = plt.subplots()
+ax.set_xlim(-180,180) # Limit of longitude
+ax.set_ylim(-90,90) # Limit of latitude
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
+ax.set_title('* Landsat 8 flying path for dotay * See the paths list below \n %s'%(today_path))
+
 l, = plt.plot([], [], 'o',color='k',markersize=2,alpha=0.1)
-plt.xlim(-180,180) # Limit of longitude
-plt.ylim(-90,90) # Limit of latitude
-line_static = FuncAnimation(fig, update_line, nframe, fargs=(xs, ys, l, False),
-                                   interval=10)
+#Global taked scene by landsat 8
+sat_taked = FuncAnimation(fig, update_sat, nframe, fargs=(xs, ys, l, False), interval=2)
 
 # -----------------------------------------------------------------------------
 # Real time visite of landsat 8
@@ -48,17 +50,16 @@ wrs=wrs[wrs.PATH.isin(today_path)]
 xs = np.array(wrs.loc[:,'CenterLong'])
 ys = np.array(wrs.loc[:,'CenterLat'])
 
-fig.add_subplot(111)
-n_drops = len(xs)
+n_points = len(xs)
 # Create satelite characteristic
-satellite = np.zeros(n_drops, dtype=[('position', float, 2),
+satellite = np.zeros(n_points, dtype=[('position', float, 2),
                                       ('size',     float, 1),
                                       ('growth',   float, 1),
                                       ('color',    float, 4)])
 
 satellite['position'][:, 0] = xs
 satellite['position'][:, 1] = ys
-satellite['growth'] = np.linspace(15, 5, n_drops)
+satellite['growth'] = np.linspace(15, 5, n_points)
 
 # Construct the scatter which we will update during animation
 # as the satellite moves.
@@ -68,7 +69,7 @@ scat = plt.scatter(satellite['position'][:, 0], satellite['position'][:, 1],
 
 def update(frame_number):
     # Get an index which we can use to re-spawn the oldest point.
-    current_index = frame_number % n_drops
+    current_index = frame_number % n_points
 
     # Make all colors more transparent as time progresses.
     satellite['color'][:, 3] -= 1.0/len(satellite)
@@ -87,15 +88,12 @@ def update(frame_number):
     scat.set_offsets(satellite['position'])
 
 # Construct the animation, using the update function as the animation director.
-animation = FuncAnimation(fig, update, interval=50)
+sat_animation = FuncAnimation(fig, update, interval=10)
 
+#Satellite real position in read
 line, = plt.plot([], [], 'o',color='g',markersize=1,alpha=0.5)
-nframe=len(wrs)/len(today_path)
-nfr=len(wrs)
-line_anime = FuncAnimation(fig, update_line, nfr, fargs=(xs, ys, line, True),
-                                   interval=50)
 
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.title('Today Paths: %s [ * Landsat real time fly * ]'%(today_path))
+#Plot satellite trace in green color
+sat_trace = FuncAnimation(fig, update_sat, len(wrs), fargs=(xs, ys, line, True), interval=10)
+
 plt.show()
